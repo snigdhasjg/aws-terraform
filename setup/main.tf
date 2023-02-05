0d51c65e7cd905bdc3222e9211c984a92c9dd59d
Snigdhajyoti Ghosh <snigdhajyoti.ghosh@gmail.com>
2023-02-06 01:10:34 +0530

    OIDC policy to trigger pipeline from any branch

--- setup/main.tf
+++ setup/main.tf
@@ -48,7 +48,9 @@ resource "aws_iam_role" "github_actions_role" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            "token.actions.githubusercontent.com:sub" = "repo:snigdhasjg/aws-terraform:ref:refs/heads/main"
+          },
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:snigdhasjg/aws-terraform:*"
           }
         }
       },
