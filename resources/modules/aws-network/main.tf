8a9240ca7fa5360d9ff1918f6c5513cc91d9cb11
Snigdhajyoti Ghosh <snigdhasjg@users.noreply.github.com>
2023-11-04 23:06:39 +0530

    update acl

--- resources/modules/aws-network/main.tf
+++ resources/modules/aws-network/main.tf
@@ -137,6 +137,36 @@ resource "aws_default_security_group" "this" {
   }
 }
 
+resource "aws_default_network_acl" "default" {
+  default_network_acl_id = aws_vpc.vpc.default_network_acl_id
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "${var.tag_prefix}-default-network-acl"
+  }
+
+  lifecycle {
+    ignore_changes = [subnet_ids]
+  }
+}
+
 resource "aws_route_table_association" "public_route_table_association" {
   for_each = aws_subnet.public_subnets
 
